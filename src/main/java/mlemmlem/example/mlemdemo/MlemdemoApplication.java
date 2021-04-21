f5f31e2d37da955b25db791ef6a0057af38449f8
khaitiger <38967901+khaitiger@users.noreply.github.com>
2021-04-21 14:52:22 +0800

    First Commit

---
+++ src/main/java/mlemmlem/example/mlemdemo/MlemdemoApplication.java
@@ -0,0 +1,13 @@
+package mlemmlem.example.mlemdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlemdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlemdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mlemmlem/example/mlemdemo/MlemdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mlemmlem.example.mlemdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlemdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
